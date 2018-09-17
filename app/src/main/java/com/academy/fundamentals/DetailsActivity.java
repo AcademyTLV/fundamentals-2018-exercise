43bff7dfdca530f4352f6a6aad799b4c24f534bc
yamin elmakis <yaminelmakis@gmail.com>
2018-09-17 23:28:02 +0300

    add open youtube trailer

--- app/src/main/java/com/academy/fundamentals/DetailsActivity.java
+++ app/src/main/java/com/academy/fundamentals/DetailsActivity.java
@@ -1,7 +1,11 @@
 package com.academy.fundamentals;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        Button btnTrailer = findViewById(R.id.details_btn_trailer);
+        btnTrailer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String trailerUrl = DetailsActivity.this.getString(R.string.infinity_war_trailer);
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(trailerUrl));
+                startActivity(browserIntent);
+            }
+        });
     }
 }
