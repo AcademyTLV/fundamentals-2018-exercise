61a429d7d90cef8e4bf5656f4a0abc6c7906ebf8
yamin elmakis <yaminelmakis@gmail.com>
2018-09-20 18:15:54 +0300

    update package

--- app/src/main/java/com/academy/fundamentals/details/DetailsActivity.java
+++ app/src/main/java/com/academy/fundamentals/details/DetailsActivity.java
@@ -1,11 +1,13 @@
-package com.academy.fundamentals;
+package com.academy.fundamentals.details;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
+import com.academy.fundamentals.R;
 
 public class DetailsActivity extends AppCompatActivity {
 
